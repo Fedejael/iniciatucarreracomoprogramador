f5e4fdf07e1b2baf1dae7926e6301694e3dfe551
fedejael <federicojael.canto@gmail.com>
2023-07-01 01:43:28 -0300

    Primer Hola Mundo

--- CursoCsharp/Form1.Designer.cs
+++ CursoCsharp/Form1.Designer.cs
@@ -29,13 +29,38 @@ namespace CursoCsharp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHolamundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHolamundo
+            // 
+            this.lblHolamundo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(89)))));
+            this.lblHolamundo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHolamundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHolamundo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(46)))), ((int)(((byte)(124)))));
+            this.lblHolamundo.Location = new System.Drawing.Point(0, 0);
+            this.lblHolamundo.Name = "lblHolamundo";
+            this.lblHolamundo.Size = new System.Drawing.Size(800, 450);
+            this.lblHolamundo.TabIndex = 0;
+            this.lblHolamundo.Text = "Hola mundo";
+            this.lblHolamundo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHolamundo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHolamundo;
     }
 }
 
