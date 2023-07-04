6ebd21de7185b66f9f2fc97446908158707aa765
fedejael <federicojael.canto@gmail.com>
2023-07-03 23:55:23 -0300

    Ejercicio3 completo

--- CursoCsharp/Ejercicios/Ejercicio3.Designer.cs
+++ CursoCsharp/Ejercicios/Ejercicio3.Designer.cs
@@ -30,6 +30,22 @@ namespace CursoCsharp.Ejercicios
         private void InitializeComponent()
         {
             this.lblejercicio = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblimc = new System.Windows.Forms.Label();
+            this.lblcondicion = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblejercicio
@@ -39,20 +55,187 @@ namespace CursoCsharp.Ejercicios
             this.lblejercicio.ForeColor = System.Drawing.Color.White;
             this.lblejercicio.Location = new System.Drawing.Point(0, 0);
             this.lblejercicio.Name = "lblejercicio";
-            this.lblejercicio.Size = new System.Drawing.Size(890, 80);
+            this.lblejercicio.Size = new System.Drawing.Size(998, 80);
             this.lblejercicio.TabIndex = 2;
             this.lblejercicio.Text = "Ejercicio 3";
             this.lblejercicio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::CursoCsharp.Properties.Resources.Ejercicio_3;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(998, 294);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(198)))), ((int)(((byte)(40)))));
+            this.label2.Location = new System.Drawing.Point(0, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(998, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Solucion";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btncalcular);
+            this.panel1.Controls.Add(this.lblcondicion);
+            this.panel1.Controls.Add(this.lblimc);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.txtaltura);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.txtpeso);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 420);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(998, 350);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(29, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 26);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Peso(kg)=";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(156, 66);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(90, 1);
+            this.panel3.TabIndex = 9;
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.txtpeso.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpeso.ForeColor = System.Drawing.Color.White;
+            this.txtpeso.Location = new System.Drawing.Point(156, 41);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(90, 22);
+            this.txtpeso.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(156, 110);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(90, 1);
+            this.panel4.TabIndex = 20;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.txtaltura.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtaltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtaltura.ForeColor = System.Drawing.Color.White;
+            this.txtaltura.Location = new System.Drawing.Point(156, 85);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(90, 22);
+            this.txtaltura.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(29, 84);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 26);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Altura(m)=";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(81, 145);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 26);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "IMC=";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(18, 181);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(132, 26);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Condicion=";
+            // 
+            // lblimc
+            // 
+            this.lblimc.AutoSize = true;
+            this.lblimc.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblimc.ForeColor = System.Drawing.Color.Lime;
+            this.lblimc.Location = new System.Drawing.Point(156, 145);
+            this.lblimc.Name = "lblimc";
+            this.lblimc.Size = new System.Drawing.Size(20, 26);
+            this.lblimc.TabIndex = 24;
+            this.lblimc.Text = "-";
+            // 
+            // lblcondicion
+            // 
+            this.lblcondicion.AutoSize = true;
+            this.lblcondicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcondicion.ForeColor = System.Drawing.Color.Yellow;
+            this.lblcondicion.Location = new System.Drawing.Point(156, 181);
+            this.lblcondicion.Name = "lblcondicion";
+            this.lblcondicion.Size = new System.Drawing.Size(20, 26);
+            this.lblcondicion.TabIndex = 25;
+            this.lblcondicion.Text = "-";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(89)))));
+            this.btncalcular.FlatAppearance.BorderSize = 0;
+            this.btncalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(46)))), ((int)(((byte)(124)))));
+            this.btncalcular.Location = new System.Drawing.Point(280, 68);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(152, 42);
+            this.btncalcular.TabIndex = 13;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
             // 
             // Ejercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblejercicio);
             this.Name = "Ejercicio3";
-            this.Size = new System.Drawing.Size(890, 588);
+            this.Size = new System.Drawing.Size(998, 770);
             this.Load += new System.EventHandler(this.Ejercicio3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +243,19 @@ namespace CursoCsharp.Ejercicios
         #endregion
 
         private System.Windows.Forms.Label lblejercicio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lblcondicion;
+        private System.Windows.Forms.Label lblimc;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.Label label1;
     }
 }
--- CursoCsharp/Ejercicios/Ejercicio3.cs
+++ CursoCsharp/Ejercicios/Ejercicio3.cs
@@ -15,10 +15,68 @@ namespace CursoCsharp.Ejercicios
         {
             InitializeComponent();
         }
+        double peso;
+        double altura;
+        double imc;       
         public string titulo;
         private void Ejercicio3_Load(object sender, EventArgs e)
         {
             lblejercicio.Text = titulo;
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            Asignardatos();
+            ObtenerIMC();
+            Condicionar();
+
+        }
+        private void Asignardatos()
+        {
+            try
+            {
+                peso = Convert.ToDouble(txtpeso.Text);
+                altura = Convert.ToDouble(txtaltura.Text);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+        private void ObtenerIMC()
+        {
+            try
+            {
+                imc = peso / Math.Pow(altura, 2);
+                lblimc.Text = imc.ToString(("N2"));
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+            }
+          
+        }
+        private void Condicionar()
+        {
+            if(imc < 18.5)
+            {
+                lblcondicion.Text = "Por debajo";
+            }
+            if(imc >= 18.5 && imc <= 24.9)
+            {
+                lblcondicion.Text = "Saludable";
+            }
+            if(imc >= 25 && imc <= 29.9)
+            {
+                lblcondicion.Text = "Sobrepeso";
+            }
+            if(imc > 30)
+            {
+                lblcondicion.Text = "Obesidad";
+            }
         }
     }
 }
